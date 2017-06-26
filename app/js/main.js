f72fcde23f2fc02993fa2ec43ba28e7a87cec22a
jeremygholland <jeremygholland@gmail.com>
2017-06-25 20:21:29 -0500

    added a side navigation for mobile

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,11 @@ var app = angular.module('app', ['ui.router', 'firebase'])
 
 app.controller('myCtrl', ['$scope', '$firebase', '$firebaseArray', function($scope, $firebase, $firebaseArray){
 
-
+  $('.button-collapse').sideNav({
+      menuWidth: 250, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  });
 //nav bar function note
 
 $(function(){
@@ -36,10 +40,20 @@ $(window).height()
 
 new WOW().init();
 
+var width = $(window).width(); 
+
+if (width >768){
     $('.timeline').timelify({
         animRight: "fadeInRight",
         animCenter: "zoomIn"
     })
+    }
+    else{
+          $('.timeline').timelify({
+        animRight: "fadeInRight",
+        animLeft: "fadeInRight"
+    })
+    }
 
 
 
